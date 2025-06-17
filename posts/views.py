b2ef5ca7204fa95776aafe82db86ba8112b428da
ru482008 <ru482008@gmail.com>
2025-06-17 17:00:29 +0800

    20250617

--- posts/views.py
+++ posts/views.py
@@ -25,7 +25,17 @@ from datetime import datetime
 def index_use_template(requests):
     now = datetime.now()
     article_records = Post.objects.all()
-    list_data = ["1", "2", "3"]
+    # list_data = ["1", "2", "3"]
     # dict_data = {'key':1,'key':1,}
     # return render(requests, "index.html", {'now':now})
-    return render(requests, "index.html", locals())
+    # return render(requests, "index.html", locals())
+    return render(requests, "pages/home.html", locals())
+
+
+def showPost(requests, slug):
+    article = Post.objects.get(slug=slug)
+    return render(requests, "pages/post.html", locals())
+
+
+def login(requests):
+    return render(requests, "pages/login.html")
