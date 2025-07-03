0076b17e73a4efeeed93567a420824ed86498dec
ru482008 <ru482008@gmail.com>
2025-07-03 09:48:47 +0800

    20250703 download

---
+++ download_db.bat
@@ -0,0 +1,1 @@
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1DZT0QISHTQ66k-Q2PX_M84KHQnccRsJs" -O db.sqlite3
---
+++ download_db.sh
@@ -0,0 +1,2 @@
+rm -rf db.sqlite3
+wget --no-check-certificate "https://docs.google.com/uc?export=download&id=1DZT0QISHTQ66k-Q2PX_M84KHQnccRsJs" -O db.sqlite3
