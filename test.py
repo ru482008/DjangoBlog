412c1bf8b2c703758e6c6d5a942b806ceaa4f304
ru482008 <ru482008@gmail.com>
2025-07-14 12:06:31 +0800

    20250714

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from modules.crawler.utils import parserTargetURL
 
 
-print(parserTargetURL('board', board='Baseball'))
-print(parserTargetURL('board',
-                      board='Baseball',
-                      pageNum='19621'))
-print(parserTargetURL('article'))
-print(parserTargetURL('article',
-                      articleURL='/bbs/Baseball/M.1752411597.A.6CA.html'))
+# print(parserTargetURL('board', board='Baseball'))
+# print(parserTargetURL('board',
+#                       board='Baseball',
+#                       pageNum='19621'))
+# print(parserTargetURL('article'))
+# print(parserTargetURL('article',
+#                       articleURL='/bbs/Baseball/M.1752411597.A.6CA.html'))
 
 
 from modules.crawler.utils import run
